bea62f93b9192ee0819d3f101d6ea7e5e30d13b8
Daksh <daksh@technowolf.in>
2024-07-14 20:56:14 +0530

    feat: Migrate to Material 3 and setup temporary color schemes
    The following changes were made:
    
    - Updated dependencies to use Material 3 components.
    - Replaced Material 2 components with their Material 3 equivalents.
    - Updated theme definitions to use Material 3 color schemes.
    - Add temporary colorscheme for dark and light mode
    - Added support for dynamic colors on Android 12+.

--- composeApp/src/wasmJsMain/kotlin/main.kt
+++ composeApp/src/wasmJsMain/kotlin/main.kt
@@ -1,3 +1,4 @@
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeViewport
 import kotlinx.browser.document
@@ -5,6 +6,9 @@ import kotlinx.browser.document
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
     ComposeViewport(document.body!!) {
-        App()
+        App(
+            darkTheme = isSystemInDarkTheme(),
+            dynamicColor = false,
+        )
     }
 }
